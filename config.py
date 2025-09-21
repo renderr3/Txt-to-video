6febdb5d7a378d4003730a59624cf64cc96711af
renderr3 <rendergithub3@gmail.com>
2025-09-21 22:12:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,11 +8,12 @@ class Config(object):
     # get a token from @BotFather
     pass
     """
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    API_ID =int(os.environ["API_ID", ""
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8271214309:AAG5QCxq5MOtWzJqgcVwxcMnxJLQPv0SU7A")
+    API_ID =int(os.environ["API_ID", "25225538"
     ]
-    API_HASH = os.environ["API_HASH", ""]
+    API_HASH = os.environ["API_HASH", "66d401695a3326c406e8782fa26d158d"]
     AUTH_USERS = "8075836012"
-    AUTH_GROUP = """"
+    AUTH_GROUP = ""
+
 
 
